e03390fd7a271c6c4c12b53e7782da16bd78cbf2
unknown <allaclass21@gmail.com>
2024-04-02 05:26:26 +0900

    [Style] 변수 alla6 변경

--- ver.6.0.1/js/alla-class-post-common-6.0.1.js
+++ ver.6.0.1/js/alla-class-post-common-6.0.1.js
@@ -1,22 +1,39 @@
 // [함수 정의] 유저가이드 이미지 표시
-let fnAllaGuideDivImagesInput = () => {
-    let allaGuideDivImages = document.querySelectorAll('.allaGuideDiv img');
-    allaGuideDivImages.forEach(function(img) {
+let fnalla6GuideDivImagesInput = () => {
+    let alla6GuideDivImages = document.querySelectorAll('.alla6GuideDiv img');
+    alla6GuideDivImages.forEach(function (img) {
         img.setAttribute('src', 'https://tistory2.daumcdn.net/tistory/4700529/skin/images/Blog_Guide_Web.gif');
     });
 }
 
 // [함수 정의] 객관식 답안 클릭 이벤트
-let fnAllaBasicTblInputsClick = () => {
-    let allaBasicTblInputsClick = document.querySelectorAll('table.allaBasicTbl .allaAnswerTr input[type="radio"]');
-    allaBasicTblInputsClick.forEach(function(input) {
-        input.addEventListener('click', function() {
-            // .allaAnswerTr / backgroundColor:gray / on
+let fnalla6BasicTblInputsClick = () => {
+    // 특정 클래스를 가진 테이블 내의 라디오 버튼들을 선택합니다.
+    let alla6BasicTblInputsClick = document.querySelectorAll('table.alla6BasicTbl .alla6AnswerTr input[type="radio"]');
+    // 각 라디오 버튼에 대해 반복합니다.
+    alla6BasicTblInputsClick.forEach(function (input) {
+        // 라디오 버튼에 클릭 이벤트를 추가합니다.
+        input.addEventListener('click', function () {
+            // 클릭된 라디오 버튼의 부모인 테이블 행(<tr>)을 선택합니다.
             let parentTr = this.parentElement.parentElement.parentElement;
+            // 클릭된 행과 같은 레벨의 다른 테이블 행들을 선택합니다.
             let siblingsTrs = parentTr.parentElement.children;
+
+            // 모든 형제 행들의 배경색을 초기화합니다.
             for (let i = 0; i < siblingsTrs.length; i++) {
+                // 마지막 행인 경우 배경색을 변경하지 않습니다.
+                if (siblingsTrs[i].classList.contains('alla6SolveTr')) {
+                    continue;
+                }
+                if (siblingsTrs[i].classList.contains('alla6ExampleTr_Txt')) {
+                    continue;
+                }
+                if (siblingsTrs[i].classList.contains('alla6ExampleTr_Img')) {
+                    continue;
+                }
                 siblingsTrs[i].querySelector('td').style.backgroundColor = 'rgba(220, 220, 220, 0)';
             }
+            // 클릭된 행의 배경색을 변경합니다.
             parentTr.querySelector('td').style.backgroundColor = 'rgba(220, 220, 220, 0.7)';
         });
     });
@@ -25,27 +42,28 @@ let fnAllaBasicTblInputsClick = () => {
 // [함수 정의] 문제 정답 수집 및 객체 생성 = { 문제번호 : 정답 }
 let fnQuestionAnswers = () => {
     // 정답 부분 가져오기
-    let answerText = document.querySelector('.allaAnswerTableDiv table tbody tr:nth-child(2) td:nth-child(1)').textContent.trim();
+    let answerText = document.querySelector('.alla6AnswerTableDiv table tbody tr:nth-child(2) td:nth-child(1)').textContent.trim();
 
     // 객체 생성
     let questionsAnswers = {};
 
-    // 모든 .allaQuestionNo 클래스를 가진 요소 선택
-    let allaQuestionNoElements = document.querySelectorAll('.allaQuestionNo');
+    // 모든 .alla6QuestionNo 클래스를 가진 요소 선택
+    let alla6QuestionNoElements = document.querySelectorAll('.alla6QuestionNo');
 
     // 각 요소의 내용을 배열에 담기
-    allaQuestionNoElements.forEach(function(element) {
-        let questionNo = element.textContent.trim(); // 문제 번호 가져오기
-        let questionNoNumeric = Number(questionNo); // 숫자로 변환
-        if (!isNaN(questionNoNumeric)) { // 숫자로 변환 가능한 경우에만 처리
+    alla6QuestionNoElements.forEach(function (element) {
+        // 문제 번호 가져오기
+        let questionNo = element.textContent.trim();
+        // 숫자로 변환
+        let questionNoNumeric = Number(questionNo);
+        // 숫자로 변환 가능한 경우에만 처리
+        if (!isNaN(questionNoNumeric)) {
             // 정답 부분에서 해당 문제 번호의 정답을 가져와서 객체에 저장
             let answer = answerText.charAt(questionNoNumeric - 1); // 정답 가져오기
             questionsAnswers[questionNo] = answer; // 객체에 저장
         }
     });
 
-    // 결과 확인
-    // console.log(questionsAnswers);
     return questionsAnswers;
 };
 
@@ -77,11 +95,11 @@ let fnSelectedAnswers = (item, objSelectedAnswers) => {
 let fnCheckAnswers = (objQuestionAnswers, objSelectedAnswers) => {
     let objCheckedAnswers = {};
 
-    // 모든 .allaQuestionNo 클래스를 가진 요소 선택
-    let allaQuestionNoElements = document.querySelectorAll('.allaQuestionNo');
+    // 모든 .alla6QuestionNo 클래스를 가진 요소 선택
+    let alla6QuestionNoElements = document.querySelectorAll('.alla6QuestionNo');
 
     // 각 요소의 내용을 배열에 담기
-    allaQuestionNoElements.forEach(function(element) {
+    alla6QuestionNoElements.forEach(function (element) {
         let questionNo = element.textContent.trim(); // 문제 번호 가져오기
         let questionNoNumeric = Number(questionNo); // 숫자로 변환
         if (!isNaN(questionNoNumeric)) { // 숫자로 변환 가능한 경우에만 처리
@@ -99,7 +117,7 @@ let fnCheckAnswers = (objQuestionAnswers, objSelectedAnswers) => {
             // [함수 정의] 채점 빨간팬
             let fnCheckRedPen = () => {
                 for (let questionNo in objCheckedAnswers) {
-                    let tableId = 'allaBasicTbl' + questionNo;
+                    let tableId = 'alla6BasicTbl' + questionNo;
                     let tableAddr = document.getElementById(tableId);
                     let checkResult = objCheckedAnswers[questionNo];
                     if (tableAddr) {
@@ -119,7 +137,7 @@ let fnCheckAnswers = (objQuestionAnswers, objSelectedAnswers) => {
                     }
                 }
             };
-            
+
             if (qa === sa) {
                 objCheckedAnswers[questionNo] = 'O';
                 fnCheckRedPen();
@@ -185,58 +203,56 @@ let fnCheckAnswers = (objQuestionAnswers, objSelectedAnswers) => {
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // 유저가이드 이미지 표시
-    fnAllaGuideDivImagesInput();
+    fnalla6GuideDivImagesInput();
 
     // 객관식 답안 클릭 이벤트
-    fnAllaBasicTblInputsClick();
+    fnalla6BasicTblInputsClick();
 
     // 문제답안 객체 생성 = { 문제번호 : 정답 }
     let objQuestionAnswers = fnQuestionAnswers();
-    // console.log(objQuestionAnswers);
-    
 
     // 선택답안 객체 생성 = { 문제번호 : 선택답안 }
-    let allaBasicTblInputs = document.querySelectorAll('table.allaBasicTbl .allaAnswerTr input[type="radio"]');
+    let alla6BasicTblInputs = document.querySelectorAll('table.alla6BasicTbl .alla6AnswerTr input[type="radio"]');
     let objSelectedAnswers = {};
-    allaBasicTblInputs.forEach(function(input) {
-        input.addEventListener('click', function() {
+    alla6BasicTblInputs.forEach(function (input) {
+        input.addEventListener('click', function () {
             objSelectedAnswers = fnSelectedAnswers(this, objSelectedAnswers);
         });
     });
-    
+
     // 문제별 체점 버튼
     // 버튼 요소를 가져옴
-    let checkButtons = document.querySelectorAll('.allaCheckTr button');
+    let checkButtons = document.querySelectorAll('.alla6CheckTr button');
 
     // 버튼 클릭 이벤트 리스너 등록
-    checkButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
+    checkButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
             fnCheckAnswers(objQuestionAnswers, objSelectedAnswers);
         });
     });
 
     // 전체 채점 버튼
     // 버튼 요소 가져옴
-    let allCheckButton = document.querySelectorAll('.allaCheckTbl td button')[0];
+    let allCheckButton = document.querySelectorAll('.alla6CheckTbl td button')[0];
     // 버튼 클릭 이벤트 리스너 등록
-    allCheckButton.addEventListener('click', function() {
+    allCheckButton.addEventListener('click', function () {
         // 채점하기
         fnCheckAnswers(objQuestionAnswers, objSelectedAnswers);
         // 다시채점 버튼 활성화
-        let openCheckButtons = document.querySelectorAll('.allaCheckTr button');
-        openCheckButtons.forEach(function(button) {
+        let openCheckButtons = document.querySelectorAll('.alla6CheckTr button');
+        openCheckButtons.forEach(function (button) {
             button.style.display = 'inline-block';
         });
         // 해설영역 활성화
-        let openSolves = document.querySelectorAll('.allaBasicTbl .allaSolveTr td');
-        openSolves.forEach(function(solve) {
+        let openSolves = document.querySelectorAll('.alla6BasicTbl .alla6SolveTr td');
+        openSolves.forEach(function (solve) {
             solve.style.display = 'block';
         });
         // 첫번째 문제 영역으로 스크롤업
-        let firstAllaBasicTbl = document.querySelector('table.allaBasicTbl');
-        firstAllaBasicTbl.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        let firstalla6BasicTbl = document.querySelector('table.alla6BasicTbl');
+        firstalla6BasicTbl.scrollIntoView({ behavior: 'smooth', block: 'start' });
         // 바디 최상단 영역으로 스크롤업
         let bodyTop = document.querySelector('html');
         bodyTop.scrollIntoView({ behavior: 'smooth', block: 'start' });
@@ -251,9 +267,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // 티스토리 카테고리 파라미터 삭제 코드
 Array.prototype.slice.call(document.querySelectorAll('a'))
-    .filter(function(el) {
+    .filter(function (el) {
         return el.href.match(/\/[0-9]+\?category/gi);
     })
-    .forEach(function(el) {
+    .forEach(function (el) {
         el.href = el.href.replace(/\?category.*/, '');
     });
