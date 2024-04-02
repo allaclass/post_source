75ce04f349a873baf08527888827b1d44aa3b570
unknown <allaclass21@gmail.com>
2024-04-02 14:01:43 +0900

    [Style] 코드 정리

--- ver.6.0.1/js/alla-class-post-common-6.0.1.js
+++ ver.6.0.1/js/alla-class-post-common-6.0.1.js
@@ -1,5 +1,5 @@
 // [함수 정의] 유저가이드 이미지 표시
-let fnalla6GuideDivImagesInput = () => {
+let fnAlla6GuideDivImagesInput = () => {
     let alla6GuideDivImages = document.querySelectorAll('.alla6GuideDiv img');
     alla6GuideDivImages.forEach(function (img) {
         img.setAttribute('src', 'https://tistory2.daumcdn.net/tistory/4700529/skin/images/Blog_Guide_Web.gif');
@@ -7,7 +7,7 @@ let fnalla6GuideDivImagesInput = () => {
 }
 
 // [함수 정의] 객관식 답안 클릭 이벤트
-let fnalla6BasicTblInputsClick = () => {
+let fnAlla6BasicTblInputsClick = () => {
     // 특정 클래스를 가진 테이블 내의 라디오 버튼들을 선택합니다.
     let alla6BasicTblInputsClick = document.querySelectorAll('table.alla6BasicTbl .alla6AnswerTr input[type="radio"]');
     // 각 라디오 버튼에 대해 반복합니다.
@@ -39,17 +39,12 @@ let fnalla6BasicTblInputsClick = () => {
     });
 }
 
-// [함수 정의] 문제 정답 수집 및 객체 생성 = { 문제번호 : 정답 }
-let fnQuestionAnswers = () => {
-    // 정답 부분 가져오기
-    let answerText = document.querySelector('.alla6AnswerTableDiv table tbody tr:nth-child(2) td:nth-child(1)').textContent.trim();
-
+// [함수 정의] 유효 문제 번호 수집 및 배열 할당 = ['01', '02', ...]
+let fnCollectQuestionNo = () => {
     // 객체 생성
-    let questionsAnswers = {};
-
+    let arrCollectQuestionNo = [];
     // 모든 .alla6QuestionNo 클래스를 가진 요소 선택
     let alla6QuestionNoElements = document.querySelectorAll('.alla6QuestionNo');
-
     // 각 요소의 내용을 배열에 담기
     alla6QuestionNoElements.forEach(function (element) {
         // 문제 번호 가져오기
@@ -58,13 +53,49 @@ let fnQuestionAnswers = () => {
         let questionNoNumeric = Number(questionNo);
         // 숫자로 변환 가능한 경우에만 처리
         if (!isNaN(questionNoNumeric)) {
-            // 정답 부분에서 해당 문제 번호의 정답을 가져와서 객체에 저장
-            let answer = answerText.charAt(questionNoNumeric - 1); // 정답 가져오기
-            questionsAnswers[questionNo] = answer; // 객체에 저장
+            arrCollectQuestionNo.push(questionNo); // 객체에 저장
         }
     });
+    return arrCollectQuestionNo;
+};
 
+// [함수 정의] 문제 정답 수집 및 객체 생성 = { 문제번호 : 정답 }
+let fnQuestionAnswers = () => {
+    // 정답 부분 가져오기
+    let answerText = document.querySelector('.alla6AnswerTableDiv table tbody tr:nth-child(2) td:nth-child(1)').textContent.trim();
+
+    // 객체 생성
+    let questionsAnswers = {};
+
+    // 유효 문제 번호 가져오기
+    let arrCollectQuestionNo = fnCollectQuestionNo();
+
+    // 정답 가져와서 객체에 담기
+    for (let i=0; i < arrCollectQuestionNo.length; i++) {
+        let questionAnswer = answerText.charAt(i);  // 정답 가져오기
+        questionsAnswers[arrCollectQuestionNo[i]] = questionAnswer; // 객체에 문제번호:정답 셋팅
+    }
+
+    // 반환
     return questionsAnswers;
+};
+
+// [함수 정의] 문제 선택답안 초기값 셋팅 = { 문제번호 : 'N' }
+let fnNonSelectedAnswers = () => {
+    // 객체 생성
+    let nonSelectedAnswers = {};
+
+    // 유효 문제 번호 가져오기
+    let arrCollectQuestionNo = fnCollectQuestionNo();
+
+    // 빈선택지 객체에 담기
+    for (let i=0; i < arrCollectQuestionNo.length; i++) {
+        nonSelectedAnswers[arrCollectQuestionNo[i]] = 'N'; // 객체에 문제번호:'N' 셋팅
+    }
+    console.log('nonSelectedAnswers(onload-fn): ', nonSelectedAnswers)
+
+    // 반환
+    return nonSelectedAnswers;
 };
 
 // [함수 정의] 문제 선택답안 수집 및 객체 생성 = { 문제번호 : 선택답안 }
@@ -84,176 +115,191 @@ let fnSelectedAnswers = (item, objSelectedAnswers) => {
             objSelectedAnswers[questionNo] = answerValue;
         }
         // 결과 확인
-        // console.log(objSelectedAnswers);
+        console.log('objSelectedAnswers(click): ', objSelectedAnswers);
     }
 
     // 선택된 답안 객체를 리턴합니다.
     return objSelectedAnswers;
 };
 
+
+
+// [함수 정의] 채점 빨간팬
+let fnCheckRedPen = (questionNo, checkResult) => {
+    let tableId = 'alla6BasicTbl' + questionNo;
+    let tableAddr = document.getElementById(tableId);
+    console.log('checkResult: ', checkResult)
+    if (tableAddr) {
+        switch (checkResult) {
+            case 'O':
+                tableAddr.style.backgroundImage = 'url(https://tistory1.daumcdn.net/tistory/4700529/skin/images/check-O.png)';
+                break;
+            case 'X':
+                tableAddr.style.backgroundImage = 'url(https://tistory2.daumcdn.net/tistory/4700529/skin/images/check-X.png)';
+                break;
+            case 'N':
+                tableAddr.style.backgroundImage = 'url(https://tistory3.daumcdn.net/tistory/4700529/skin/images/check-V.png)';
+                break;
+            default:
+                tableAddr.style.backgroundImage = 'url(https://tistory3.daumcdn.net/tistory/4700529/skin/images/check-V.png)';
+        }
+        tableAddr.style.backgroundRepeat = 'no-repeat';
+        tableAddr.style.backgroundSize = '7.0em';
+        tableAddr.style.backgroundPosition = 'top left';
+    }
+};
+
 // [함수 정의] 채점 기능
 let fnCheckAnswers = (objQuestionAnswers, objSelectedAnswers) => {
-    let objCheckedAnswers = {};
+    // 객체 생성
+    let objCheckedResult = {};
+    // 채점 객체 초기화
+    // - 유효 문제 번호 가져오기
+    let arrCollectQuestionNo = fnCollectQuestionNo();
+    // - 빈선택지 객체에 담기
+    for (let i=0; i < arrCollectQuestionNo.length; i++) {
+        // objCheckedResult[arrCollectQuestionNo[i]] = 'N'; // 객체에 문제번호:'N' 셋팅
+        // console.log('objCheckedResult(onload): ', objCheckedResult)
+        let questionNo = arrCollectQuestionNo[i];
+        let checkResult;
+        let qa = objQuestionAnswers[questionNo];
+        let sa = objSelectedAnswers[questionNo];
 
-    // 모든 .alla6QuestionNo 클래스를 가진 요소 선택
-    let alla6QuestionNoElements = document.querySelectorAll('.alla6QuestionNo');
-
-    // 각 요소의 내용을 배열에 담기
-    alla6QuestionNoElements.forEach(function (element) {
-        let questionNo = element.textContent.trim(); // 문제 번호 가져오기
-        let questionNoNumeric = Number(questionNo); // 숫자로 변환
-        if (!isNaN(questionNoNumeric)) { // 숫자로 변환 가능한 경우에만 처리
-            // 유효 문제 번호 키마다 N값 설정
-            let tempAnswer = 'N';
-            objCheckedAnswers[questionNo] = tempAnswer; // 객체에 저장
-        }
-        // objQuestionAnswers와 objSelectedAnswers의 키를 기준으로 값을 비교하여 objCheckedAnswers 객체 생성
-        for (let questionNo in objQuestionAnswers) {
-            // objQuestionAnswers와 objSelectedAnswers의 키가 일치하고 값도 일치하는지 확인
-            // 값이 동일한 경우 'O', 동일하지 않은 경우 'X'로 objCheckedAnswers 객체에 저장
-            let qa = objQuestionAnswers[questionNo];
-            let sa = objSelectedAnswers[questionNo];
-
-            // [함수 정의] 채점 빨간팬
-            let fnCheckRedPen = () => {
-                for (let questionNo in objCheckedAnswers) {
-                    let tableId = 'alla6BasicTbl' + questionNo;
-                    let tableAddr = document.getElementById(tableId);
-                    let checkResult = objCheckedAnswers[questionNo];
-                    if (tableAddr) {
-                        switch (checkResult) {
-                            case 'O':
-                                tableAddr.style.backgroundImage = 'url(https://tistory1.daumcdn.net/tistory/4700529/skin/images/check-O.png)';
-                                break;
-                            case 'X':
-                                tableAddr.style.backgroundImage = 'url(https://tistory2.daumcdn.net/tistory/4700529/skin/images/check-X.png)';
-                                break;
-                            default:
-                                tableAddr.style.backgroundImage = 'url(https://tistory3.daumcdn.net/tistory/4700529/skin/images/check-V.png)';
-                        }
-                        tableAddr.style.backgroundRepeat = 'no-repeat';
-                        tableAddr.style.backgroundSize = '7.0em';
-                        tableAddr.style.backgroundPosition = 'top left';
-                    }
-                }
-            };
-
-            if (qa === sa) {
-                objCheckedAnswers[questionNo] = 'O';
-                fnCheckRedPen();
-            } else {
-                switch (qa) {
-                    case 'A':
-                        objCheckedAnswers[questionNo] = (sa === '1' || sa === '2') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    case 'B':
-                        objCheckedAnswers[questionNo] = (sa === '1' || sa === '3') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    case 'C':
-                        objCheckedAnswers[questionNo] = (sa === '1' || sa === '4') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    case 'D':
-                        objCheckedAnswers[questionNo] = (sa === '2' || sa === '3') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    case 'E':
-                        objCheckedAnswers[questionNo] = (sa === '2' || sa === '4') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    case 'F':
-                        objCheckedAnswers[questionNo] = (sa === '3' || sa === '4') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    case 'G':
-                        objCheckedAnswers[questionNo] = (sa === '1' || sa === '2' || sa === '3') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    case 'H':
-                        objCheckedAnswers[questionNo] = (sa === '1' || sa === '2' || sa === '4') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    case 'I':
-                        objCheckedAnswers[questionNo] = (sa === '1' || sa === '3' || sa === '4') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    case 'J':
-                        objCheckedAnswers[questionNo] = (sa === '2' || sa === '3' || sa === '4') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    case 'K':
-                        objCheckedAnswers[questionNo] = (sa === '1' || sa === '2' || sa === '3' || sa === '4') ? 'O' : 'X';
-                        fnCheckRedPen();
-                        break;
-                    default:
-                        objCheckedAnswers[questionNo] = 'X';
-                        fnCheckRedPen();
-                }
+        if (sa === 'N') {
+            objCheckedResult[questionNo] = 'N';
+            checkResult = objCheckedResult[questionNo];
+            fnCheckRedPen(questionNo, checkResult);
+        } else if (qa === sa) {
+            objCheckedResult[questionNo] = 'O';
+            checkResult = objCheckedResult[questionNo];
+            fnCheckRedPen(questionNo, checkResult);
+        } else {
+            switch (qa) {
+                case 'A':
+                    objCheckedResult[questionNo] = (sa === '1' || sa === '2') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                case 'B':
+                    objCheckedResult[questionNo] = (sa === '1' || sa === '3') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                case 'C':
+                    objCheckedResult[questionNo] = (sa === '1' || sa === '4') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                case 'D':
+                    objCheckedResult[questionNo] = (sa === '2' || sa === '3') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                case 'E':
+                    objCheckedResult[questionNo] = (sa === '2' || sa === '4') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                case 'F':
+                    objCheckedResult[questionNo] = (sa === '3' || sa === '4') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                case 'G':
+                    objCheckedResult[questionNo] = (sa === '1' || sa === '2' || sa === '3') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                case 'H':
+                    objCheckedResult[questionNo] = (sa === '1' || sa === '2' || sa === '4') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                case 'I':
+                    objCheckedResult[questionNo] = (sa === '1' || sa === '3' || sa === '4') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                case 'J':
+                    objCheckedResult[questionNo] = (sa === '2' || sa === '3' || sa === '4') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                case 'K':
+                    objCheckedResult[questionNo] = (sa === '1' || sa === '2' || sa === '3' || sa === '4') ? 'O' : 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
+                    break;
+                default:
+                    objCheckedResult[questionNo] = 'X';
+                    checkResult = objCheckedResult[questionNo];
+                    fnCheckRedPen(questionNo, checkResult);
             }
-            // if (objQuestionAnswers.hasOwnProperty(questionNo) && objSelectedAnswers.hasOwnProperty(questionNo)) {
-            // }
         }
-    });
-
+    }
     // 결과 확인
-    // console.log(objCheckedAnswers);
-    return objCheckedAnswers;
-}
+    console.log('objCheckedResult(click): ', objCheckedResult);
+    return objCheckedResult;
+};
 
 
 document.addEventListener('DOMContentLoaded', function () {
     // 유저가이드 이미지 표시
-    fnalla6GuideDivImagesInput();
+    fnAlla6GuideDivImagesInput();
 
     // 객관식 답안 클릭 이벤트
-    fnalla6BasicTblInputsClick();
+    fnAlla6BasicTblInputsClick();
 
     // 문제답안 객체 생성 = { 문제번호 : 정답 }
     let objQuestionAnswers = fnQuestionAnswers();
+    console.log('questionAnswers(onload): ', objQuestionAnswers);
 
     // 선택답안 객체 생성 = { 문제번호 : 선택답안 }
+    // - input[type="radio"]를 모두 불러옴
     let alla6BasicTblInputs = document.querySelectorAll('table.alla6BasicTbl .alla6AnswerTr input[type="radio"]');
-    let objSelectedAnswers = {};
+    // - 초기에 선택된 답안이 없을 경우 'N'으로 초기셋팅
+    let objSelectedAnswers = {...fnNonSelectedAnswers()};
+    console.log('objSelectedAnswers(onload): ', objSelectedAnswers);
+    // - 각 라디오 버튼 요소에 대해 변경 이벤트 리스너를 설정
     alla6BasicTblInputs.forEach(function (input) {
-        input.addEventListener('click', function () {
+        input.addEventListener('change', function () {
+            // 라디오 버튼이 클릭되면 선택된 답안을 처리하고 결과를 objSelectedAnswers에 할당
             objSelectedAnswers = fnSelectedAnswers(this, objSelectedAnswers);
         });
     });
 
     // 문제별 체점 버튼
-    // 버튼 요소를 가져옴
-    let checkButtons = document.querySelectorAll('.alla6CheckTr button');
+    // - 버튼 요소를 가져옴
+    let alla6checkButtons = document.querySelectorAll('.alla6CheckTr button');
 
-    // 버튼 클릭 이벤트 리스너 등록
-    checkButtons.forEach(function (button) {
+    // - 버튼 클릭 이벤트 리스너 등록
+    alla6checkButtons.forEach(function (button) {
         button.addEventListener('click', function () {
             fnCheckAnswers(objQuestionAnswers, objSelectedAnswers);
         });
     });
 
     // 전체 채점 버튼
-    // 버튼 요소 가져옴
+    // - 버튼 요소 가져옴
     let allCheckButton = document.querySelectorAll('.alla6CheckTbl td button')[0];
-    // 버튼 클릭 이벤트 리스너 등록
+    // - 버튼 클릭 이벤트 리스너 등록
     allCheckButton.addEventListener('click', function () {
-        // 채점하기
+        // - 채점하기
         fnCheckAnswers(objQuestionAnswers, objSelectedAnswers);
-        // 다시채점 버튼 활성화
+        // - 다시채점 버튼 활성화
         let openCheckButtons = document.querySelectorAll('.alla6CheckTr button');
         openCheckButtons.forEach(function (button) {
             button.style.display = 'inline-block';
         });
-        // 해설영역 활성화
+        // - 해설영역 활성화
         let openSolves = document.querySelectorAll('.alla6BasicTbl .alla6SolveTr td');
         openSolves.forEach(function (solve) {
             solve.style.display = 'block';
         });
-        // 첫번째 문제 영역으로 스크롤업
-        let firstalla6BasicTbl = document.querySelector('table.alla6BasicTbl');
-        firstalla6BasicTbl.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        // 바디 최상단 영역으로 스크롤업
+        // - 첫번째 문제 영역으로 스크롤업
+        // let firstalla6BasicTbl = document.querySelector('table.alla6BasicTbl');
+        // firstalla6BasicTbl.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        // - 바디 최상단 영역으로 스크롤업
         let bodyTop = document.querySelector('html');
         bodyTop.scrollIntoView({ behavior: 'smooth', block: 'start' });
     });
