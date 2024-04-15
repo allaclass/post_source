a71743eae6056e9d7632defae9784f3fa23fc2b0
unknown <allaclass21@gmail.com>
2024-04-15 22:28:05 +0900

    [Comment] 주석 정리

---
+++ adsenseTest/alla-class-adsense.js
@@ -0,0 +1,53 @@
+var table10 = document.querySelector('table#allaBasicTbl10');
+if (table10) {
+  var divAd10 = document.createElement('div');
+  divAd10.id = 'ad10';
+  divAd10.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3055418534570705" data-ad-slot="3845370848" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+  table10.appendChild(divAd10);
+  (adsbygoogle = window.adsbygoogle || []).push({});
+}
+
+var table20 = document.querySelector('table#allaBasicTbl20');
+if (table20) {
+  var divAd20 = document.createElement('div');
+  divAd20.id = 'ad20';
+  divAd20.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3055418534570705" data-ad-slot="4476760384" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+  table20.appendChild(divAd20);
+  (adsbygoogle = window.adsbygoogle || []).push({});
+}
+
+var table30 = document.querySelector('table#allaBasicTbl30');
+if (table30) {
+  var divAd30 = document.createElement('div');
+  divAd30.id = 'ad30';
+  divAd30.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3055418534570705" data-ad-slot="6071045099" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+  table30.appendChild(divAd30);
+  (adsbygoogle = window.adsbygoogle || []).push({});
+}
+
+var table45 = document.querySelector('table#allaBasicTbl45');
+if (table45) {
+  var divAd45 = document.createElement('div');
+  divAd45.id = 'ad45';
+  divAd45.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3055418534570705" data-ad-slot="3845370848" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+  table45.appendChild(divAd45);
+  (adsbygoogle = window.adsbygoogle || []).push({});
+}
+
+var table55 = document.querySelector('table#allaBasicTbl55');
+if (table55) {
+  var divAd55 = document.createElement('div');
+  divAd55.id = 'ad55';
+  divAd55.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3055418534570705" data-ad-slot="4476760384" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+  table55.appendChild(divAd55);
+  (adsbygoogle = window.adsbygoogle || []).push({});
+}
+
+var table65 = document.querySelector('table#allaBasicTbl65');
+if (table65) {
+  var divAd65 = document.createElement('div');
+  divAd65.id = 'ad65';
+  divAd65.innerHTML = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3055418534570705" data-ad-slot="6071045099" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+  table65.appendChild(divAd65);
+  (adsbygoogle = window.adsbygoogle || []).push({});
+}
